9f9b38f3f1d7e011a57b5feaa1c30c487437db44
ttaka66 <d.m_ro.nl.e@icloud.com>
2017-07-23 02:25:54 +0900

    Show url to console

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var http = require("http"); // httpはコアモジュール
 var server = http.createServer();
 
+var url = require("url");
+
 server.on("request", function(req, res){
-	console.log("request caught");
-	res.writeHead(200);
-	res.end("Hello, World!");
+	// console.log("request caught");
+	// res.writeHead(200);
+	// res.end("Hello, World!");
+	console.log(req.url);
 });
 
 server.listen(3000);
