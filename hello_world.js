415647f00729519470a5fba22fb4afb51b180211
ttaka66 <d.m_ro.nl.e@icloud.com>
2017-07-22 21:06:43 +0900

    Installed nodemon

--- hello_world.js
+++ hello_world.js
@@ -1,1 +1,5 @@
-console.log("Hello, World");
+// nodemonは変更時に再起動する
+// (注)グローバル(プロジェクト単位ではなく)にインストールしなければならない
+// (インストール) $ npm install nodemon -g
+// (起動) $ nodemon {ファイル名}
+console.log("Hello, World!");
