50ac038f1882aacda0f384b77a224dabeb56b867
ttaka66 <d.m_ro.nl.e@icloud.com>
2017-07-23 13:48:10 +0900

    Added express server

---
+++ expressproject/server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+
+app.get("/hello", function(req, res){
+	res.send("Hello, world!");
+});
+
+app.get("/goodbye", function(req, res){
+	res.send("Good-bye, world!");
+});
+
+app.all("*", function(req, res){
+	res.sendStatus(404);
+});
+
+app.listen(3000, function(){
+	console.log("Running on port 3000")
+});
