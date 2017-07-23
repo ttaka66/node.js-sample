7ab3fb901794ca1fadd246276197dcd8b7fb356b
ttaka66 <d.m_ro.nl.e@icloud.com>
2017-07-23 12:01:51 +0900

    Changed response by url

--- server.js
+++ server.js
@@ -10,6 +10,17 @@ server.on("request", function(req, res){
 	console.log(req.url);
 	var incomingURL = url.parse(req.url);
 	console.log(incomingURL);
+
+	if (incomingURL.path === "/hello") {
+		res.writeHead(200);
+		res.end("Hello, World!");
+	} else if (incomingURL.path === "/goodbye"){
+		res.writeHead(200);
+		res.end("Good-bye, World!");
+	}else {
+		res.writeHead(200);
+		res.end("Resource not found.");
+	}
 });
 
 server.listen(3000);
